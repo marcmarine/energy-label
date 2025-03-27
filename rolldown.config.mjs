5b11c6874e3f2b721fa03fe9043f8032c172bf65
Marc Mariné <shenobi@gmail.com>
2025-03-27 14:36:36 +0100

    refactor: Add external dependencies handling

--- rolldown.config.mjs
+++ rolldown.config.mjs
@@ -1,6 +1,13 @@
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { readFileSync } from 'node:fs'
 import { defineConfig } from 'rolldown'
 
-const external = ['lit-html', '@lit-labs/ssr', 'qrcode', 'react', 'svgo']
+const projectRoot = dirname(fileURLToPath(import.meta.url))
+const pkgPath = join(projectRoot, 'package.json')
+const packageJson = JSON.parse(readFileSync(pkgPath, 'utf-8'))
+
+const external = ['svgo/browser', ...Object.keys(packageJson.dependencies)]
 
 export default defineConfig([
   {
