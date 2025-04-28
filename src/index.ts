6271aaf54339db9e361c14920624c057d53204fd
Marc Mariné <shenobi@gmail.com>
2025-04-28 23:08:21 +0200

    fix: Correct types for `createEnergyLabel` function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import EnergyLabel from './EnergyLabel'
-import type { EnergyLabelOptions, EURegulationAct } from './definitions'
+import type { EURegulationAct, LabelRegulationMap } from './definitions'
 export { EU_REGULATION } from './constants'
 
-export function createEnergyLabel(regulation: EURegulationAct, options?: Partial<EnergyLabelOptions>) {
+export function createEnergyLabel<T extends EURegulationAct>(regulation: T, options?: Partial<LabelRegulationMap>) {
   return new EnergyLabel(regulation, options)
 }
 
+export * from './definitions'
 export default EnergyLabel
