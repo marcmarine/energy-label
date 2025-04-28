95571e57eebf701785b241d5a26a3f9005a91b42
Marc Mariné <shenobi@gmail.com>
2025-04-28 23:04:28 +0200

    fix: Remove test item from EU regulations

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,5 @@
 export const EU_REGULATION = {
   '2019/2016/2023-09-30': {
     title: 'Household refrigerating appliances'
-  },
-  '2019/2016/2023-09-31': {
-    title: 'Household refrigerating appliances'
   }
 }
