b0a4396c8fa19de83f5f252d6b05622122569400
Marc Mariné <shenobi@gmail.com>
2025-06-08 21:48:09 +0200

    test: Update export test for new modules

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,7 @@ import * as index from './index'
 
 describe('Index', () => {
   it('should export certain submodules', () => {
-    ;['default', 'createEnergyLabel', 'LabelDOMRenderer'].forEach(moduleName => {
+    ;['default', 'createEnergyLabel', 'appendTo', 'download'].forEach(moduleName => {
       expect(Object.keys(index)).toContain(moduleName)
     })
   })
